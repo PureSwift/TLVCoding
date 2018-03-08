bd7d3c815989d6b8ca28ec47290881605278a5ae
Alsey Coleman Miller <alseycmiller@gmail.com>
2018-03-08 16:59:31 -0500

    Added unit test

--- Tests/TLVCodingTests/TLVCodingTests.swift
+++ Tests/TLVCodingTests/TLVCodingTests.swift
@@ -8,16 +8,102 @@
 
 import Foundation
 import XCTest
-import TLVCoding
+@testable import TLVCoding
 
-class TLVCodingTests: XCTestCase {
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        //// XCTAssertEqual(TLVCoding().text, "Hello, World!")
-    }
+final class TLVCodingTests: XCTestCase {
     
     static var allTests = [
-        ("testExample", testExample),
-    ]
+        ("testCodable", testCodable),
+        ]
+    
+    func testCodable() {
+        
+        let data = Data([1, 1, 0, 0, 7, 67, 111, 108, 101, 109, 97, 110])
+        
+        guard let value = Person(data: data)
+            else { XCTFail("Could decode"); return }
+        
+        XCTAssert(value.data == data, "Could not encode")
+        
+        XCTAssert(value == Person(gender: .male, name: "Coleman"))
+    }
+}
+
+// MARK: - Supporting Types
+
+public struct Person: Equatable {
+    
+    public enum Gender: UInt8 {
+        
+        case male
+        case female
+    }
+    
+    public var gender: Gender
+    
+    public var name: String
+    
+    public init(gender: Gender, name: String) {
+        
+        self.gender = gender
+        self.name = name
+    }
+    
+    public static func == (lhs: Person, rhs: Person) -> Bool {
+        
+        return lhs.gender == rhs.gender
+            && lhs.name == rhs.name
+    }
+    
+    public init?(data: Data) {
+        
+        guard let fields = try? TLVDecoder.decode(data: data, from: (TLVField.Gender.self, TLVField.Name.self))
+            else { return nil }
+        
+        self.gender = fields.0.rawValue
+        self.name = fields.1.rawValue
+    }
+    
+    public var data: Data {
+        
+        let fields: [TLVEncodable] = [
+            TLVField.Gender(rawValue: gender),
+            TLVField.Name(rawValue: name)
+        ]
+        
+        return TLVEncoder.encode(fields)
+    }
+    
+    private enum TLVField {
+        
+        enum TypeCode: UInt8, TLVTypeCode {
+            
+            case name
+            case gender
+        }
+        
+        struct Name: TLVCodable, RawRepresentable {
+            
+            static let typeCode: TLVTypeCode = TypeCode.name
+            
+            var rawValue: String
+            
+            init(rawValue: String) {
+                
+                self.rawValue = rawValue
+            }
+        }
+        
+        struct Gender: TLVCodable, RawRepresentable {
+            
+            static let typeCode: TLVTypeCode = TypeCode.gender
+            
+            var rawValue: Person.Gender
+            
+            init(rawValue: Person.Gender) {
+                
+                self.rawValue = rawValue
+            }
+        }
+    }
 }
