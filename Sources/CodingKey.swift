b62d05bc9a6ca438ce5ac01081fb3f5363c872c9
Alsey Coleman Miller <alseycmiller@gmail.com>
2019-05-13 23:57:49 -0500

    Fixed Swift 4 compilation

--- Sources/CodingKey.swift
+++ Sources/CodingKey.swift
@@ -94,11 +94,21 @@ internal extension CodingKey {
     static var sanitizedName: String {
         
         let rawName = String(reflecting: self)
+        #if swift(>=5.0)
         var elements = rawName.split(separator: ".")
+        #else
+        var elements = rawName.components(separatedBy: ".")
+        #endif
         guard elements.count > 2
             else { return rawName }
         elements.removeFirst()
+        #if swift(>=5.0)
         elements.removeAll { $0.contains("(unknown context") }
+        #else
+        while let index = elements.index(where: { $0.contains("(unknown context") }) {
+            elements.remove(at: index)
+        }
+        #endif
         return elements.reduce("", { $0 + ($0.isEmpty ? "" : ".") + $1 })
     }
 }
