26eddf20cc7b2095dfcbf883fcbaeeda19cb11f4
Alsey Coleman Miller <alseycmiller@gmail.com>
2019-12-21 22:57:42 -0500

    Added `TLVDateFormat`

--- Sources/Format.swift
+++ Sources/Format.swift
@@ -20,9 +20,30 @@ public enum TLVUUIDFormat: Equatable, Hashable {
     case string
 }
 
+/// TLV Date Encoding Format
+public enum TLVDateFormat: Equatable, Hashable {
+    
+    /// The strategy that encodes dates in terms of seconds since midnight UTC on January 1, 1970.
+    case secondsSince1970
+    
+    /// The strategy that encodes dates in terms of milliseconds since midnight UTC on January 1, 1970.
+    case millisecondsSince1970
+    
+    /// The strategy that formats dates according to the ISO 8601 and RFC 3339 standards.
+    case iso8601
+    
+    /// The strategy that defers formatting settings to a supplied date formatter.
+    case formatted(DateFormatter)
+    
+    /// The strategy that formats custom dates by calling a user-defined function.
+    case custom((Date, Encoder) -> Void)
+}
+
 internal struct TLVOptions {
     
     let numericFormat: TLVNumericFormat
     
     let uuidFormat: TLVUUIDFormat
+    
+    let dateFormat: TLVDateFormat
 }
