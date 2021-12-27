85180c2faeb65c6380903df0607d12870a14d3cb
Alsey Coleman Miller <alseycmiller@gmail.com>
2021-12-27 16:53:57 -0500

    Added WebAssembly support

--- Sources/Data.swift
+++ Sources/Data.swift
@@ -14,15 +14,12 @@ internal extension Data {
         
         // stored in heap, can reuse buffer
         if count > Data.inlineBufferSize {
-            
             return withUnsafeBytes { (buffer: UnsafeRawBufferPointer) in
                 Data(bytesNoCopy: UnsafeMutableRawPointer(mutating: buffer.baseAddress!.advanced(by: range.lowerBound)),
                      count: range.count,
                      deallocator: .none)
             }
-            
         } else {
-            
             // stored in stack, must copy
             return subdata(in: range)
         }
@@ -33,7 +30,6 @@ internal extension Data {
     }
     
     func suffixCheckingBounds(from start: Int) -> Data {
-        
         if count > start {
             return Data(suffix(from: start))
         } else {
@@ -54,9 +50,11 @@ private extension Data {
         #if arch(x86_64) || arch(arm64) || arch(arm64_32) || arch(s390x) || arch(powerpc64) || arch(powerpc64le)
         typealias Buffer = (UInt8, UInt8, UInt8, UInt8, UInt8, UInt8, UInt8, UInt8,
             UInt8, UInt8, UInt8, UInt8, UInt8, UInt8) //len  //enum
-        #elseif arch(i386) || arch(arm)
+        #elseif arch(i386) || arch(arm) || arch(wasm32)
         typealias Buffer = (UInt8, UInt8, UInt8, UInt8,
             UInt8, UInt8)
+        #else
+        #error("Platform not supported")
         #endif
         
         return MemoryLayout<Buffer>.size
