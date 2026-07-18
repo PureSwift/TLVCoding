778a76831652aaca7ed1efa30ca8bfa3e88f9420
Alsey Coleman Miller <alseycmiller@gmail.com>
2026-07-18 00:22:11 -0400

    Update Package.swift for Swift 6 and macro support

--- Package.swift
+++ Package.swift
@@ -1,13 +1,25 @@
-// swift-tools-version:5.1
+// swift-tools-version:6.0
 import PackageDescription
+import CompilerPluginSupport
 import class Foundation.ProcessInfo
 
+// get environment variables
+let environment = ProcessInfo.processInfo.environment
+let dynamicLibrary = environment["SWIFT_BUILD_DYNAMIC_LIBRARY"] != nil
+let enableMacros = environment["SWIFTPM_ENABLE_MACROS"] != "0"
+let buildDocs = environment["BUILDING_FOR_DOCUMENTATION_GENERATION"] != nil
+
 // force building as dynamic library
-let dynamicLibrary = ProcessInfo.processInfo.environment["SWIFT_BUILD_DYNAMIC_LIBRARY"] != nil
 let libraryType: PackageDescription.Product.Library.LibraryType? = dynamicLibrary ? .dynamic : nil
 
 var package = Package(
     name: "TLVCoding",
+    platforms: [
+        .macOS(.v10_15),
+        .iOS(.v13),
+        .watchOS(.v6),
+        .tvOS(.v13),
+    ],
     products: [
         .library(
             name: "TLVCoding",
@@ -17,8 +29,7 @@ var package = Package(
     ],
     targets: [
         .target(
-            name: "TLVCoding",
-            path: "./Sources"
+            name: "TLVCoding"
         ),
         .testTarget(
             name: "TLVCodingTests",
@@ -27,12 +38,76 @@ var package = Package(
     ]
 )
 
-// SwiftPM command plugins are only supported by Swift version 5.6 and later.
-#if swift(>=5.6)
-let buildDocs = ProcessInfo.processInfo.environment["BUILDING_FOR_DOCUMENTATION_GENERATION"] != nil
+// SwiftPM plugins
 if buildDocs {
     package.dependencies += [
-        .package(url: "https://github.com/apple/swift-docc-plugin", from: "1.0.0"),
+        .package(
+            url: "https://github.com/swiftlang/swift-docc-plugin.git",
+            from: "1.4.5"
+        )
     ]
 }
-#endif
+
+if enableMacros {
+    let version: Version
+    #if swift(>=6.3)
+    version = "603.0.1"
+    #elseif swift(>=6.2)
+    version = "602.0.0"
+    #elseif swift(>=6.1)
+    version = "601.0.1"
+    #else
+    version = "600.0.1"
+    #endif
+    package.targets[0].swiftSettings = [
+        .define("SWIFTPM_ENABLE_MACROS")
+    ]
+    package.dependencies += [
+        .package(
+            url: "https://github.com/swiftlang/swift-syntax.git",
+            from: version
+        )
+    ]
+    package.targets += [
+        .macro(
+            name: "TLVCodingMacros",
+            dependencies: [
+                .product(
+                    name: "SwiftSyntaxMacros",
+                    package: "swift-syntax"
+                ),
+                .product(
+                    name: "SwiftCompilerPlugin",
+                    package: "swift-syntax"
+                )
+            ]
+        )
+    ]
+    package.targets[0].dependencies += [
+        "TLVCodingMacros"
+    ]
+    package.targets += [
+        .testTarget(
+            name: "TLVCodingMacrosTests",
+            dependencies: [
+                "TLVCodingMacros",
+                .product(
+                    name: "SwiftSyntaxMacros",
+                    package: "swift-syntax"
+                ),
+                .product(
+                    name: "SwiftSyntaxMacroExpansion",
+                    package: "swift-syntax"
+                ),
+                .product(
+                    name: "SwiftParser",
+                    package: "swift-syntax"
+                ),
+                .product(
+                    name: "SwiftSyntaxMacrosTestSupport",
+                    package: "swift-syntax"
+                )
+            ]
+        )
+    ]
+}
